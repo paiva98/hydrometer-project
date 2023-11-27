b99e461600853bd467e432b4989c87d3fe67ac2a
paiva98 <nilsonpaiva98@gmail.com>
2023-11-27 03:04:09 -0300

    new display

--- frontend/src/components/HidroHome.vue
+++ frontend/src/components/HidroHome.vue
@@ -27,6 +27,17 @@
                 <!-- <HydrometerValues :values="hydrometer.predictions" /> -->
             </div>
         </div>
+        <div>
+            <div class="circulo">
+                <div class="helice"></div>
+                <div class="mostrador" id="mostrador">
+                <!-- números pretos do mostrador -->
+                <span class="preto" id="preto">00000</span>
+                <!-- números vermelhos do mostrador -->
+                <span class="vermelho" id="vermelho">000</span>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -81,5 +92,63 @@ export default {
     display: flex;
     flex-direction: column;
     align-items: center;
+}
+
+/* Estilo do círculo do relógio de água */
+.circulo {
+    width: 200px;
+    height: 200px;
+    border: 10px solid black;
+    border-radius: 50%;
+    position: relative;
+    background-color: white;
+}
+
+/* Estilo da hélice do relógio de água */
+.helice {
+    width:80px;
+    height: 80px;
+    /* background: url("https://th.bing.com/th/id/OIP.1zMQXywLnvbBhR7IvF7SiwHaF4?rs=1&pid=ImgDetMain"); imagem da hélice */
+    background: url("helice.png"); /* imagem da hélice */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    animation: girar 2s linear infinite; /* animação para girar a hélice */
+}
+
+/* Estilo do mostrador do relógio de água */
+.mostrador {
+    width: 100px;
+    height: 50px;
+    border: 2px solid black;
+    position: absolute;
+    bottom: 30px;
+    left: 50%;
+    transform: translateX(-50%);
+    font-family: Arial, sans-serif;
+    font-size: 20px;
+    text-align: center;
+    line-height: 50px;
+}
+
+/* Estilo dos números pretos do mostrador */
+.preto {
+    color: black;
+}
+
+/* Estilo dos números vermelhos do mostrador */
+.vermelho {
+    color: red;
+}
+
+/* Estilo da animação para girar a hélice */
+@keyframes girar {
+    from {
+    transform: translate(-50%, -50%) rotate(0deg);
+    }
+    to {
+    transform: translate(-50%, -50%) rotate(360deg);
+    }
 }
 </style>
